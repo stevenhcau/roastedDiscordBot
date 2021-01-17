54291c51192a85bdfa9615b7ea507366acab22a9
Steven <stevenhcau@gmail.com>
2021-01-17 15:11:56 -0700

    Updated the roastedBot.py to include more logging information and set up the Bot instance to form the connection between discord.py and Discord.

--- bot/roastedBot.py
+++ bot/roastedBot.py
@@ -8,7 +8,58 @@ This is a discord bot for the roasted server.
 '''
 
 import discord
+from discord.ext.commands import Bot
 from dotenv import load_dotenv
+import logging
 import os
 
-client = discord.Client()
+DESCRIPTION = "This is a discord bot for the roasted server."
+
+# Sets up where the files will be
+ABS_PATH = os.path.abspath(__file__)
+D_NAME = os.path.dirname(ABS_PATH)
+currentDir = os.getcwd()
+
+if currentDir != D_NAME:
+    os.chdir(D_NAME)
+else:
+    pass
+
+# Sets up the logging that writes the logs to a file called discord.log instead of outputting it to the console
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+
+logger.addHandler(handler)
+
+logger.debug(f'ABS_PATH: {ABS_PATH}')
+logger.debug(f'D_NAME: {D_NAME}')
+logger.debug(f'Current Directory: {currentDir}')
+
+# Grabs discord token from .env
+load_dotenv(".env")
+TOKEN = os.getenv('DISCORD_TOKEN')
+logger.debug(f'Token: {TOKEN}')
+
+# Define intents. Intents use flags to determine what part of discord.py must be run
+intents = discord.Intents.default()
+intents.typing = True
+intents.presences = False
+intents.members = True
+intents.guilds = True
+intents.guild_messages = True
+
+# Create a bot instance - bot instances are technically Client instances, this serves as the connection from Discord to discord.py
+bot = Bot(command_prefix='!', description=DESCRIPTION, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('------')
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(TOKEN)
