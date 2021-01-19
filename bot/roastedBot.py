a1da9124afe1bf6ce0775559cc103c9c9c03dc46
Steven <stevenhcau@gmail.com>
2021-01-18 20:39:13 -0700

    Changed permissions in the discord server as an administrator and implementing a system where users who join the server have to accept the rules before getting assigned to a 'Member' role. The role assignment has not been coded into the bot, but the structure is complete. Requirements.txt also has been updated.

--- bot/roastedBot.py
+++ bot/roastedBot.py
@@ -49,6 +49,7 @@ intents.typing = True
 intents.presences = False
 intents.members = True
 intents.guilds = True
+intents.reactions = True
 intents.guild_messages = True
 
 # Create a bot instance - bot instances are technically Client instances, this serves as the connection from Discord to discord.py
@@ -56,10 +57,51 @@ bot = Bot(command_prefix='!', description=DESCRIPTION, intents=intents)
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity = discord.Activity(
+                          type = discord.ActivityType.listening, 
+                          name = 'Pretty Savage'))
     print('------')
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+
+# This awaits the "Hello" message in any chat and then replies with "Hello World!"
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    if message.author == bot.user:
+        return    
+    if message.content == "Hello":
+        await message.channel.send("Hello World!")
+    if message.content == "Bye":
+        await message.channel.send("See you!")
+
+    # This checks if the message is a DM and then replies to the author in the same channel
+    # isinstance returns True if the object argument is an instance of the classinfo argument - isinstance(object,classinfo)
+    if isinstance(message.channel, discord.DMChannel) and message.author != bot.user:
+        if message.content == "!yes":
+            await message.channel.send('Welcome to the "roasted" server!')
+        else:
+            await message.channel.send('Please read the rules and reply with "!yes"')
+
+# !server command displays the below information
+@bot.command(name='server', help='fetches server information')
+async def fetchServerInfo(context):
+	guild = context.guild
+	
+	await context.send(f'Server Name: {guild.name}')
+	await context.send(f'Server Size: {len(guild.members)}')
+	await context.send(f'Administrator Name: {guild.owner.display_name}')
+
+# Bot event that sends a DM to a user when the join the server
+@bot.event
+async def on_member_join(member): 
+    print(f'{member.name} has joined the server...')
+    print(f'{member.name} ID: {member.id}')    
+    print(f'Sending DM to {member.name}')
+
+    welcomeMessage = "Welcome to this server. Please reply with read the rules below and reply with '!yes' to join the server."
+    await member.send(content=welcomeMessage)
 
 bot.run(TOKEN)
